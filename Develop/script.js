e25ee51f5bbc963c6025d9ce6ee85d49a2a886a9
Oscar Silva <oscarsilva2356@gmail.com>
2021-12-23 22:38:22 -0800

    push with Read.me

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,17 @@
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var specials = [" ", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "\`", "{", "|", "}", "~", "\'", "\""];
-var allowedChars = [];
-var pwLength = 0;
+ // Add an array of multipe stings of the alphabet, intengers, and symbols for the generator can make a randomized password. 
+ var key_strings = {
+ lowercase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+ uppercase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+ numbers: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+ symbols: [" ", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ":", ">", "?", "@", "[", "\\", "]", "^", "_", "\`", "{", "|", "}", "~", "\'", "\""],
 
+ }
 
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-const key_strings = {
-  lowercase: 'abcdefghijklmnopqrstuvwxyz',
-  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  number: '0123456789',
-  symbol: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
-};
-
+// Add a function with if and else statemets for the user can go throught selected option to make a password. 
 function generatePassword() {
   var password = "password";
   // TODO: add code to generate the password here
@@ -42,17 +37,17 @@ function generatePassword() {
     generateBtn += key_strings.number;
   };
   var password = "";
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < key_strings.length; i++) {
     password += key_strings[Math.floor(Math.random() * generateBtn.length)]
   }
   return password;
 }
+// After these steps I was not able to make the generator to output any password yet. I could not find what was going on with the code. 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
@@ -64,35 +59,4 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-//function generatePassword() {
-  //var passwordCharSet = "";
 
- // var length = window.prompt("Enter a number from 8 to 128 for password length.");
-
- // var lowercase = window.confirm("Would you like to use lowercase letters?");
-  //if (lowercase) {
-  //  passwordCharSet += key_strings.lowercase;
-  //};
-
- // var uppercase = window.confirm("Would you like to use uppercase letters?");
-  //if (uppercase) {
-   // passwordCharSet += key_strings.uppercase;
-  //};
-
-  //var symbols = window.confirm("Would you like to use symbols?");
-  //if (symbols) {
-    //passwordCharSet += key_strings.symbol;
-  //};
-
-  //var numbers = window.confirm("Would you like to use numbers?");
-  //if (numbers) {
-    //passwordCharSet += key_strings.number;
-  //};
-  //var password = "";
-  //for (let i = 0; i < length; i++) {
-    //password += passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)]
-  //}
-  //return password;
-//}
-
-//console.log(generatePassword());
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # bc-Homework3
+
+
+In this homework assigment I had some trouble figuring out how to make the output to make the password work.  
+
+On line 2 I added a order list that filled it with arrays with each of the letters of the alphabet in order for the function below to generate from that. 
+
+From lines 15 to 43, I added lines of code with if statements and if else statements in order to have all of the required promts the pop up with the coresponding code. 
+
+At the end of the code I could not figure out what exacly why my code was not working properly. I needed help with the final touches. 
+
